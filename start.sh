75675cd8c87eb64b94e8a0ff7ba9cb164218d049
Tushar <tusharbansal@iitbhilai.ac.in>
2024-04-06 15:30:43 +0530

    fix:try3

--- start.sh
+++ start.sh
@@ -4,8 +4,14 @@ set -e
 
 echo "run db migration"
 
+
+
+cat /app/main
 cat /app/sample.env
 source /app/sample.env
+
+echo "$DB_DRIVER"
+echo "$DB_SOURCE"
 
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
