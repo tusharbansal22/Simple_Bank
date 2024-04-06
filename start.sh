ae0ee0bb17f4a9a2197e549c4d69f9ddf2f73a28
Tushar <tusharbansal@iitbhilai.ac.in>
2024-04-06 16:03:30 +0530

    fix:try6

--- start.sh
+++ start.sh
@@ -8,6 +8,11 @@ echo "run db migration"
 cat /app/sample.env
 source /app/sample.env
 ls /app
+echo "DB_SOURCE=postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable
+DB_DRIVER=postgres
+SERVER_ADDRESS=0.0.0.0:8080
+TOKEN_SYMMETRIC_KEY=tushar22tushar22tushar22tushar22
+ACCESS_TOKEN_DURATION=15m" > sample.env
 
 echo "$DB_DRIVER"
 echo "$DB_SOURCE"
